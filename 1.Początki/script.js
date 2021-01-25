a4bbfa9a6cfde1c8ff203a56e60f24e068274c02
marcixdify <43642904+marcixdify@users.noreply.github.com>
2021-01-25 17:24:44 +0100

    zadanie1

---
+++ 1.Początki/script.js
@@ -0,0 +1,18 @@
+// zadanie 1
+console.log("Marcin");
+
+//zadanie 2
+
+console.log(12 + 32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+// zadanie 3 
+//poprawne wyświetlenie
+console.log("Hello World");
+
+// nieznana zmienna
+console.log(Hello World);
+
+//błąd, niezakończony string
+console.log("Hello World);
