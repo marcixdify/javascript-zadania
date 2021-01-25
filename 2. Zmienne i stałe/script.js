52b24e67ffb40fa91ca2e2a3c9e006b3a1ad5598
marcixdify <43642904+marcixdify@users.noreply.github.com>
2021-01-25 17:36:16 +0100

    zad2

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,29 @@
+//zadanie 1
+
+const number = 7;
+const string = "marcin";
+const numberString = 2 + "marcin";
+const bool = true;
+const exampleNull = null;
+
+console.log("number:", number);
+console.log("string:", string);
+console.log("numberString:", numberString);
+console.log("bool:", bool);
+console.log("null:", exampleNull);
+
+
+// zadanie 2
+
+let x = 22;
+let y = 8;
+let sum = 0;
+sum = x + y;
+console.log(sum);
+
+// zadanie 3
+
+const variable;
+
+//występuje undefined, ponieważ żadna wartość nie została przypisana do zmiennej
+console.log(variable);
