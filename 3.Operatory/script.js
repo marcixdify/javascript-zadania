92f89f617faee964ee8bbc1ca826aa3a8392b95b
marcixdify <43642904+marcixdify@users.noreply.github.com>
2021-01-25 17:50:50 +0100

    zad3

---
+++ 3.Operatory/script.js
@@ -0,0 +1,44 @@
+// zadanie 1
+let x = true;
+let y = false;
+
+console.log(x === y);
+
+//zadanie 2
+let x = 43;
+let y = 2;
+let moduloResult = 0;
+moduloResult = x % y;
+console.log(moduloResult);
+
+//zadanie 3
+let firstString = "super";
+let secondString = "wsei";
+let stringsResult = "";
+stringsResult = firstString + secondString;
+
+console.log(stringsResult);
+
+//zadanie 4
+var someNumber = 425;
+var someString = "425";
+
+//true, wartości są takie same
+console.log(someNumber == someString);
+
+//false, poniewaz mają rózny typ zmiennej
+console.log(someNumber === someString);
+
+//zadanie 5
+let counter = 30;
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+//zadanie 6
+let x = 9;
+let y = 1;
+let result = null;
+result = x > y;
+console.log(result);
